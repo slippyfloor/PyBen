d0c28b79b89ae67393cf63489a0e7baabab846cf
slippy <98895662+slippyfloor@users.noreply.github.com>
2022-03-25 20:45:10 -0400

    Named foo back to BenMsg

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import random
 
 print("Ben?")
-foo = ['Yes', 'No', 'Eaugh', 'Hohoho!']
+BenMsg = ['Yes', 'No', 'Eaugh', 'Hohoho!']
 while True:
   benin = input()
   if benin == "":
     pass
   else:
-    print(random.choice(foo))
+    print(random.choice(BenMsg))
